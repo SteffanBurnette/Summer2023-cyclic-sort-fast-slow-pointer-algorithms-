1f9a085649427e4759aa9451b2417f5a19e2720a
Steffan Burnette <burnettesteffansep7@gmail.com>
2023-07-09 14:38:13 -0400

    Updated subarraySum

--- subarraySum.js
+++ subarraySum.js
@@ -1,36 +1,43 @@
 //Set B #6
 
-nums = [1,1,1];
-nums2 = [1,2,3];
-nums3=[2,2,3,4,4,5,7];
-nums4=[7,1,3,10,1,5,2,9,2,6,8,5,4];
 
-function subArray(ary, k){
+function subArray(arr, k) {
+    let count = 0;
+    let total = 0;
+    let end = arr.length - 1;
+    for (let i = 0; i <= end; i++) {
+      // Set j to the next index of i
+      let j = i + 1;
+      // Calculate the initial total by adding the current element and the next element
+      total = arr[i] + arr[j];
+      // Check if the current element itself is equal to k
+      if (arr[i] === k)
+        count++;
+      // Continue iterating while j is within the array bounds and the total is less than or equal to k
+      // or greater than k
+      while (j <= end && (total <= k || total >= k)) {
+        // If the total is equal to k, increment the count
+        if (total === k)
+          count++;
+        // Increment j and add the next element to the total
+        total += arr[++j];
+      }
+    }
+    // Return the count of subarrays that sum up to k
+    return count;
+  }
 
+nums = [1, 1, 1];
+nums2 = [1, 2, 3];
+nums3 = [2, 2, 3, 4, 4, 5, 7];
+nums4 = [1, -1, 0];
+nums5 = [28,54,7,-70,22,65,-6];
 
-        let count=0;
-        for(let i=1;i<ary.length;i++){
-            let j=i-1;//slow pointer
-            //let correctIndex = ary[i] - 1;
-    
-            //Checks to see if the sum equals k, or if the whole element itself equals k which then increases the counter.
-            if(ary[i]+ary[j]==k){
-             count++;
-             if(ary[i]=k){
-                count++;
-            }
-            }
-
-          
-        }
-       //Returns the number of potential instances of k.
-       return count;
-    
-}
-
-console.log(subArray(nums,2));
-console.log(subArray(nums2,3));
-console.log(subArray(nums3,7));
+console.log(subArray(nums, 2));
+console.log(subArray(nums2, 3));
+console.log(subArray(nums3, 7));
+console.log(subArray(nums4, 0));
+console.log(subArray(nums5, 100));
 
 //Exports the function to be used in other files.
 module.exports ={ subArray};
--- subarraySum.test.js
+++ subarraySum.test.js
@@ -1,13 +1,26 @@
 const { subArray } = require("./subarraySum");
 
 describe("subArray", () => {
-    test("[2,2,3,4,4,5,7] => 2", () => {
+    test("[2,2,3,4,4,5,7] => 3", () => {
     
-      expect(subArray([2,2,3,4,4,5,7],7)).toEqual(2);
+      expect(subArray([2,2,3,4,4,5,7],7)).toEqual(3);
     });
   
     test("[1,2,3] => 2", () => {
     
         expect(subArray([1,2,3],3)).toEqual(2);
+      });
+
+      test("[1, -1, 0],0 => 3", () => {
+    
+        expect(subArray([1, -1, 0],0)).toEqual(3);
+      });
+      test(" [1, 1, 1],2 => 2", () => {
+    
+        expect(subArray( [1, 1, 1],2)).toEqual(2);
+      });
+      test("[28,54,7,-70,22,65,-6],100 => 1", () => {
+    
+        expect(subArray([28,54,7,-70,22,65,-6],100)).toEqual(1);
       });
   })
